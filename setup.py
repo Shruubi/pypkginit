87b9b4e191685d86006cdfd5cee19b13f6ae587b
Damon Swayn <dswayn@email.com>
2013-02-02 01:53:56 +1100

    init commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pypkginit',
+    version='1.0',
+    author='Damon Swayn',
+    author_email='dswayn@deakin.edu.au',
+    packages=['pypkginit'],
+    url='',
+    license='LICENSE.txt',
+    description='Automates python package generation',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
